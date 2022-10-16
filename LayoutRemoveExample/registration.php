a8f44a6e6046892eddefeb089f241210a9a90e45
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-10-16 17:01:42 +0530

    Delete LayoutRemoveExample directory

--- LayoutRemoveExample/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-use Magento\Framework\Component\ComponentRegistrar;
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Training_LayoutRemoveExample',
-    __DIR__
-);
