ceb876a9d53e1ef799ae3754a2bcc13b583bb3d5
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2022-12-09 22:37:04 +0530

    Custom Attribute
    Add custom attribute

---
+++ Custom/Attribute/Setup/UpgradeData.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Custom\Attribute\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * Upgrade data for a module
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '1.0.1', '<')) {
+            /** @var EavSetup $eavSetup */
+            $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,
+                'designer',
+                [
+                    'type' => 'varchar',
+                    'label' => 'Designer',
+                    'input' => 'text',
+                    'required' => false,
+                    'sort_order' => 50,
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                    'used_in_product_listing' => true,
+                    'group' => 'General',
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
---
+++ Custom/Attribute/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author  sshukoor
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Custom_Attribute',
+    __DIR__
+);
