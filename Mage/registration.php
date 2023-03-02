d1b0dce8823ba0d6c9f8dac3e43ce8fb4fd093fd
ATHEEQ SYED R <99461347+atheeqsyed@users.noreply.github.com>
2023-03-02 08:18:18 +0530

    Update registration.php

--- Mage/registration.php
+++ Mage/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Mage_TestAttribute',
+    'Mage_TestAttibute',
     __DIR__
 );
